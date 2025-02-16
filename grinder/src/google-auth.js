3297d7e9ed8edeb5d814cd699bd8093d39d6ab64
alexander-entin <alexander.entin@gmail.com>
2025-02-16 22:17:12 +0200

    google auth

--- grinder/src/google-auth.js
+++ grinder/src/google-auth.js
@@ -1,8 +1,8 @@
 import { JWT } from 'google-auth-library'
 
 export let auth = new JWT({
-	email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-	key: process.env.GOOGLE_SERVICE_ACCOUNT_KEY.replace(/\\n/g, '\n'),
+	email: process.env.SERVICE_ACCOUNT_EMAIL,
+	key: process.env.SERVICE_ACCOUNT_KEY.replace(/\\n/g, '\n'),
 	scopes: [
 		'https://www.googleapis.com/auth/drive',
 		'https://www.googleapis.com/auth/spreadsheets',
