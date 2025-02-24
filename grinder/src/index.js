fa719019727847a95532fc8f894632d7cf8a8dac
alexander-entin <alexander.entin@gmail.com>
2025-02-24 22:30:17 +0200

    auto

--- grinder/src/index.js
+++ grinder/src/index.js
@@ -3,8 +3,8 @@ import { sleep } from './sleep.js'
 // import { cleanup } from './1.cleanup.js'
 import { load } from './load.js'
 import { summarize } from './2.summarize.js'
-// import { slides } from './3.slides.js'
-// import { audio } from './4.audio.js'
+import { slides } from './3.slides.js'
+import { audio } from './4.audio.js'
 
 // if (process.argv[2] === 'fresh') {
 // 	cleanup()
