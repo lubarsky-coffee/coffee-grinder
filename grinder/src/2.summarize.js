d24ae14c5da35b98eba4a35e51bdfb6151e6de9e
alexander-entin <alexander.entin@gmail.com>
2025-03-06 17:25:33 +0200

    decode delay

--- grinder/src/2.summarize.js
+++ grinder/src/2.summarize.js
@@ -18,7 +18,7 @@ export async function summarize() {
 
 	let stats = { ok: 0, fail: 0 }
 	let last = {
-		urlDecode: { time: 0, delay: 30e3, increment: 900 },
+		urlDecode: { time: 0, delay: 30e3, increment: 1000 },
 		ai: { time: 0, delay: 0 },
 	}
 	for (let i = 0; i < list.length; i++) {
